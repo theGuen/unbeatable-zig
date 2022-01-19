fba3415bfe2980f33f5ed5e7534842c4eac6a6df
gs <g.steffen@portrix.net>
2022-01-19 16:35:23 +0100

    todo

--- raylibwrapper/raylibwrapper.h
+++ raylibwrapper/raylibwrapper.h
@@ -2,6 +2,7 @@
 #ifndef raylibwrapper_h
 #define raylibwrapper_h
 #include <raylib.h>
+// TODO: I am not able to pass a rectangle struct so here is a little helper to pass a pointer
 void WrapDrawRectangleRec(Rectangle* rect, Color color);
 bool WrapCheckCollisionPointRec(Vector2* vec, Rectangle* rect);
 #endif
